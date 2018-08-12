4e3a9b7f55570b794042cdc808a25181a1903e30
Riger73 <timothyn@iinet.net.au>
2018-08-12 19:02:02 +1000

    cont

--- builddb.py
+++ builddb.py
@@ -3,6 +3,6 @@ import sys
 
 conn = sqlite3.connect('a1data.db')
 with conn:
-    cursor = conn.cursor() 
+    cursor = conn.cursor()
     cursor.execute("DROP TABLE IF EXISTS ASSIGNMENT1_data")
     cursor.execute("CREATE TABLE ASSIGNMENT1_data (timestamp DATETIME, temp NUMERIC, humidity NUMERIC)")
