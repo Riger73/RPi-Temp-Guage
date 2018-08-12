6c8be21ce9adb61f9cb11a56366e7fedeafaf8b5
Riger73 <timothyn@iinet.net.au>
2018-08-12 19:21:11 +1000

    linting of db script

--- builddb.py
+++ builddb.py
@@ -4,4 +4,6 @@ conn = sqlite3.connect('a1data.db')
 with conn:
     cursor = conn.cursor()
     cursor.execute("DROP TABLE IF EXISTS ASSIGNMENT1_data")
-    cursor.execute("CREATE TABLE ASSIGNMENT1_data (timestamp DATETIME, temp NUMERIC, humidity NUMERIC)")
+    cursor.execute(
+        "CREATE TABLE ASSIGNMENT1_data (timestamp DATETIME, temp NUMERIC, humidity NUMERIC)")
+        
