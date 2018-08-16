a4d0d50e26c06ffd3915ece69eb08bf5540670c0
Timothy Novice <timothyn@iinet.net.au>
2018-08-16 14:39:42 +1000

    create database management
    Creates db if none else backs up db

---
+++ managedb.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import sqlite3 as db
+import os
+from datetime import date as label
+
+# Author - Tim Novice sn: s3572290 RMIT
+#
+# Code adapted from tutorial 4 examples using 'with' keyword
+# Establishes a database connection to a1data.db
+# and builds data fields.
+# Script should be run in the root of the directory
+# where the database lives.
+#
+conn = db.connect('a1data.db')
+dbfile = 'a1data.db'
+if(os.path.isfile('/database/a1data.db')):
+    bkdbfile = 'a1data_{}.db'.format(label.today())
+    try:
+        f = open(bkdbfile, 'w')
+    finally:
+        f.close()
+else:
+    with conn:
+        cursor = conn.cursor()
+        try:
+            cursor.execute("DROP TABLE IF EXISTS ASSIGNMENT1_data")
+            cursor.execute(
+                "CREATE TABLE ASSIGNMENT1_data (\
+                    timestamp DATETIME, temp NUMERIC, humidity NUMERIC)")
+        except db.Error as e:
+            conn.log.error("Error creating Database: Cannot write to disk!" % e)
