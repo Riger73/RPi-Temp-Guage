0d3729b0b43428f3cb3d648b32898b12a0ac77cf
Timothy Novice <timothyn@iinet.net.au>
2018-08-16 12:05:08 +1000

    Exception handling for logging function

--- temp_guage.py
+++ temp_guage.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-import sys
 from sense_hat import SenseHat
 from datetime import datetime
 from threading import Timer
 import sqlite3 as db
-from threading import Thread
+import sys
 
 # Author Tim Novice sn: s3572290 RMIT
 #
@@ -12,7 +11,6 @@ from threading import Thread
 # Returns calibrated temerature and date time
 # Cycles through periodically until receiving exit feed
 # To be used as a cron task.
-# Place in same directory as database.
 #
 sense = SenseHat()
 sense.clear()
@@ -21,15 +19,21 @@ tempds = 'a1data.db'
 
 # Write data to database
 def logData(timestamp, temp, humidity):
-    params(timestamp, temp, humidity)
-    conn = db.connect(tempds)
-    curs=conn.cursor()
-    curs.execute("INSERT INTO ASSIGNMENT1_data values(?, ?, ?)", params)
-    conn.commit()
-    conn.close()
-
+    try:
+        params = (timestamp, temp, humidity)
+        conn = db.connect(tempds)
+        curs = conn.cursor()
+        curs.execute("INSERT INTO ASSIGNMENT1_data values(?, ?, ?)", params)
+    except KeyboardInterrupt:
+        conn.commit()
+        conn.close()
+        conn.interrupt()
+    finally:
+        conn.commit()
+        conn.close()
+        print("New data written to data store...")
 # Temperature and humitity poller
-def getTempData():    
+def getTempData(): 
     temp = sense.get_temperature()
     humidity = sense.get_humidity()
     sense.show_message(
@@ -38,29 +42,25 @@ def getTempData():
     sense.show_message(
         'Humidity: {0:0.1f} *c'.format(humidity), scroll_speed = 0.05)
     sense.clear()
-    if temp & humidity is not None:
+    if temp and humidity is not None:
         timestamp = datetime.now().strftime("%H:%M")
         temp = round(temp, 1)
         humidity = round(humidity, 1)
         logData(timestamp, temp, humidity)
 
-# Thread to recursively poll every 5 milliseconds, and calls processes.
 def poll():
-    try:
-        getTempData()
-        t = Timer(0.5, poll)
-        t.start()
-    except KeyboardInterrupt:
-        sense.clear()
-        print("Thread closed").exit()
+    getTempData()
+    t = Timer(0.3, poll)
+    t.start()
 
+# Handle for thread to poll every 5 milliseconds, and calls processes.
+# Effectively functions as a main().
 def main():
-    try: 
-        # Calls a thread timer to initiate the poll
-        t = Timer(0.5, poll)
+    try:
+        t = Timer(0.3, poll)
         t.start()
     except KeyboardInterrupt:
         sense.clear()
-        print("Thread closed").exit()
-
+        print("Thread closed")
+        sys.exit()
 main()
