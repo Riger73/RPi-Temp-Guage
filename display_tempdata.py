46a1aeb63ff2217220b40525a3ed11c0e4d4ac33
Timothy Novice <timothyn@iinet.net.au>
2018-08-17 17:17:59 +1000

    construct webserver index data

--- display_tempdata.py
+++ display_tempdata.py
@@ -1,12 +1,14 @@
 #!/usr/env python 3
 import pygal
+import flask
 import json
+import os
+from datetime import datetime
 import sqlite3 as db
 from urllib.request import urlopen
-from flask import Flask
+from flask import Flask, render_template, request
 from pygal.style import DarkSolarizedStyle
 
-app = Flask(__name__)
 tempds = '/database/a1data.db'
 
 # Reads data from temp/humidity database
@@ -16,6 +18,8 @@ def readData(timestamp, temp, humidity):
         conn = db.connect(tempds)
         curs = conn.cursor()
         curs.execute("SELECT FROM ASSIGNMENT1_data values(?, ?, ?)", params)
+
+        return timestamp, temp, humidity
     # Handles db locking
     except db.OperationalError as e:
         if ("locked" in str(e)):
@@ -25,3 +29,19 @@ def readData(timestamp, temp, humidity):
     finally:
         conn.commit()
         conn.close()
+
+
+# main route 
+@app.route("/")
+def index():	
+	timestamp, temp, humidity = readData()
+	templateData = {
+		'timestamp': timestamp,
+		'temp': temp,
+        'humidity' : humidity
+	}
+	return render_template('index.html', **templateData)
+
+if __name__ == "__main__":
+	host = os.popen('hostname -I').read()
+	app.run(host=host, port=80, debug=False)
