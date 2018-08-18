9b1867c1c6e369b20b87bdaa2392025d94c69c38
Riger73 <timothyn@iinet.net.au>
2018-08-18 13:16:00 +1000

    Create linegraphing template

--- display_tempdata.py
+++ display_tempdata.py
@@ -50,26 +50,28 @@ def readData():
 
 
 # Method using Pygal libraries to plot line graphs 
+@app.route("/graphs")
 getLinegraph():
     try:
-        
-    line_chart = pygal.Line()
-    line_chart.title = 'Temperature & Humidity Data over time'
-    line_chart.x_labels = map(str, range(2002, 2013))
-    line_chart.add('Firefox', [None, None,    0, 16.6,   25,   31, 36.4, 45.5, 46.3, 42.8, 37.1])
-    line_chart.add('Chrome',  [None, None, None, None, None, None,    0,  3.9, 10.8, 23.8, 35.3])
-    line_chart.add('IE',      [85.8, 84.6, 84.7, 74.5,   66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
-    line_chart.add('Others',  [14.2, 15.4, 15.3,  8.9,    9, 10.4,  8.9,  5.8,  6.7,  6.8,  7.5])
-    line_chart.render()
+        line_chart = pygal.Line()
+        line_chart.title = 'Temperature & Humidity Data over time'
+        line_chart.x_labels = map(str, range(2002, 2013))
+        line_chart.add('Temperature', [None, None,    0, 16.6,   25,   31, 36.4, 45.5, 46.3, 42.8, 37.1])
+        line_chart.add('Humidity',  [None, None, None, None, None, None,    0,  3.9, 10.8, 23.8, 35.3])
+        line_chart.render()
+    return render_template('linegraph.html', templates)
+    except Exception, e:
+        return(str(e))
+
 
 # Main routine - Design taken from week 5 code samples  
 @app.route("/")
 def index():
     timestamp, temp, humidity = readData()
     templateData = {
-        'timestamp': timestamp,
-        'temp': temp,
-        'humidity': humidity
+        'Timestamp': timestamp,
+        'Temperature': temp,
+        'Humidity': humidity
     }
     return render_template('index.html', **templateData)
 
