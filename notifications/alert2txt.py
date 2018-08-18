c3b56e7af17a2bdb671183d6b9f10c2c931906ac
Riger73 <timothyn@iinet.net.au>
2018-08-19 09:58:52 +1000

    formatted alert script to PEP 8

---
+++ notifications/alert2txt.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import requests
+import json
+import os
+
+# Author Tim Novice sn: s3572290 RMIT
+#
+# Sends text alerts when temperature dips below 10 degrees.
+# Uses Pushbullet as text service.
+# Code leverages week 4 and 5 tutorial material - specifically
+# "96_pycurlBullet.py".
+#
+
+ACCESS_TOKEN = ""
+
+
+# Method to use PushBullet to send text alerts when the temperature
+# drops below 10.
+def alerttexter(title, body):
+    """ Sending notification via pushbullet.
+        Args:
+            title (str) : title of text.
+            body (str) : Body of text.
+    """
+    data_send = {"type": "note", "title": title, "body": body}
+
+    resp = requests.post(
+        'https://api.pushbullet.com/v2/pushes', data=json.dumps(data_send),
+        headers={
+            'Authorization': 'Bearer ' + ACCESS_TOKEN, 'Content-Type':
+            'application/json'
+        }
+    )
+    if resp.status_code != 200:
+        raise Exception('Something wrong')
+    else:
+        print('complete sending')
+
+
+# main function
+def main():
+    ip_address = os.popen('hostname -I').read()
+    alerttexter(ip_address, "From Raspberry Pi")
+
+
+main()
