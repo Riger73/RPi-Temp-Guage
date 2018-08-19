2c6be4513d0d5a2e7f47f90a2544e82bbcdf0aca
Tim Novice <timothyn@iinet.net.au>
2018-08-19 17:50:48 +1000

    Added comments

--- database/builddb.py
+++ database/builddb.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 import sqlite3 as db
 
+# Author - Tim Novice sn: s3572290 RMIT
+#
+# Code adapted from tutorial 4 examples using 'with' keyword.
+# Utility to create adhoc database.
+#
+
 conn = db.connect('a1data.db')
 with conn:
     curs = conn.cursor()
