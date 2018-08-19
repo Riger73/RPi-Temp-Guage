a8b08cef5e087dc336e3ea61ce26d4658dd36c6a
Riger73 <timothyn@iinet.net.au>
2018-08-19 17:31:53 +1000

    added webserver to cronjob setup

--- set_cronjob.py
+++ set_cronjob.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python3
 
-# Code taken from week 4 PIoT code samples - RMIT 2018
+# Code adapted from week 4 PIoT code samples - RMIT 2018
+# Author Tim Novice sn: s3572290 RMIT
+#
+# sets up application cron jobs.
+# Utility script to be run once at set up while logged in as pi.
+#
+
 from crontab import CronTab
-    
-#init cron
+
+
+# init cron daemon
 cron = CronTab(user='pi')
 cron.remove_all()
 
-#add new cron job
-temp  = cron.new(command='/database/temp_guage.py')
-database = cron.new(command='/database/builddb.py')
 
-#job settings
+# add new cron job
+webserver = cron.new(command='/home/pi/WebService/display_tempdata.py &')
+temp = cron.new(command='/database/temp_guage.py &')
+database = cron.new(command='/database/builddb.py &')
+
+
+# set job frequencies
+webserver.minute.every(15)
 temp.minute.every(15)
 database.day.every(1)
 cron.write()
